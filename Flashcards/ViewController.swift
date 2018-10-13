d02d979799a3c23e0015a5381ac564e2ab42eb29
Mengmei He <maggie@mengmeis-macbook-air.local>
2018-10-13 16:52:25 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Mengmei He on 10/13/18.
+//  Copyright © 2018 Maggie He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
